a3789c9e4890a9a60548c53450f722b5fba5905b
durjoydas1 <durjoykumardas1@outlook.com>
2022-01-25 19:29:49 +0600

    files modified

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 setInterval(mytime, 1000);
-function mytime(){
+function mytime() {
     let fulltime = new Date();
     let H = fulltime.getHours();
     let M = fulltime.getMinutes();
@@ -9,16 +9,16 @@ function mytime(){
     let date = fulltime.getDate();
     let month = fulltime.getMonth() + 1;
     let year = fulltime.getFullYear();
-    (0<=H<=12)? P = 'AM' : P = 'PM';
-    (H>12)? H = H -12 : H = H; 
-    (H<10)? document.getElementById('hour').innerHTML = "0" + H: document.getElementById('hour').innerHTML = H;
-    (M<10)? document.getElementById('minute').innerHTML = "0" + M: document.getElementById('minute').innerHTML = M;
-    (S<10)? document.getElementById('second').innerHTML = "0" + S: document.getElementById('second').innerHTML = S;
+    (H <= 12)? P = 'AM' : P = 'PM';
+    (H > 12) ? H = H - 12 : H = H;
+    (H < 10) ? document.getElementById('hour').innerHTML = "0" + H : document.getElementById('hour').innerHTML = H;
+    (M < 10) ? document.getElementById('minute').innerHTML = "0" + M : document.getElementById('minute').innerHTML = M;
+    (S < 10) ? document.getElementById('second').innerHTML = "0" + S : document.getElementById('second').innerHTML = S;
     document.getElementById('period').innerHTML = P;
-    (date<10)? date = '0'+date: date = date;
-    (month<10)? month = '0'+month: month = month;
+    (date < 10) ? date = '0' + date : date = date;
+    (month < 10) ? month = '0' + month : month = month;
     document.getElementById('date').innerHTML = `${date}/${month}/${year}`;
-    switch(D){
+    switch (D) {
         case 0:
             document.getElementById('bar').innerHTML = 'Sunday';
             break;
@@ -41,7 +41,7 @@ function mytime(){
             document.getElementById('bar').innerHTML = 'Satarday';
             break;
     }
-    
-    
+
+
 }
 
